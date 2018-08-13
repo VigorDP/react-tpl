f6986fb6220ffb5833253c3bbe85ebe397f10598
fengyinchao <1542963276@qq.com>
2018-08-13 21:54:23 +0800

    feat:规范化

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,5 +1,5 @@
-import { LOAD_JOKE, GET_MOBILE } from '../actionType'
-import { loadJokeList, getMobile } from 'api'
+import { LOAD_JOKE } from '../actionType'
+import { loadJokeList } from 'api'
 import { createAction } from 'redux-actions'
 // 语法：createAction(type, payloadCreator)
-export const getMobileAction = createAction(GET_MOBILE, () => getMobile())
+export const loadJokeListAction = createAction(LOAD_JOKE, () => loadJokeList())

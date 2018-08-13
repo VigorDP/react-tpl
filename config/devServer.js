8ba2ea19e424b5bf78d4d62cfa0359a0464e5562
fengyinchao <1542963276@qq.com>
2018-08-13 21:31:07 +0800

    feat:auto proxy

--- config/devServer.js
+++ config/devServer.js
@@ -1,29 +1,16 @@
-const HOST = require('../src/utils/env').HOST
-const env = require('../src/utils/env').ENV
-const proxyPath = getProxyPath(env)
+const HOST = require('../src/request/env').HOST
+const env = require('../src/request/env').ENV
+const proxyPath = require('../src/request/env').PROXYPATH
 
 module.exports = {
   // port: 3000,
   // contentBase: './src',
   hot: true,
   proxy: {
-    // [proxyPath]: {
-    //   target: HOST[env],
-    //   changeOrigin: true
-    // }
-    '/mobile/*': {
-      target: 'http://localhost:8227/',
-      changeOrigin: true
+    [proxyPath]: {
+      target: HOST[env],
+      changeOrigin: true,
+      pathRewrite: { [proxyPath]: '/' }
     }
   }
-}
-
-function getProxyPath(env) {
-  let proxyPath = 'devApi'
-  if (env === 'test') {
-    proxyPath = 'testApi'
-  } else if (env === 'production') {
-    proxyPath = 'prodApi'
-  }
-  return proxyPath
 }

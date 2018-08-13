3a598402c025e14883c0594645139b01a131a14d
fengyinchao <1542963276@qq.com>
2018-08-13 21:31:07 +0800

    feat:auto proxy

---
+++ src/request/env.js
@@ -0,0 +1,27 @@
+const HOST = {
+  development: 'http://v.juhe.cn/',
+  test: 'http://v.juheTest.cn/',
+  production: 'http://v.juheProd.cn/'
+}
+
+const ENV = 'development'
+// const ENV = 'test';
+// const ENV = 'production';
+
+const PROXYPATH = getProxyPath(ENV)
+
+module.exports = {
+  HOST,
+  ENV,
+  PROXYPATH
+}
+
+function getProxyPath(env) {
+  let proxyPath = '/devApi'
+  if (env === 'test') {
+    proxyPath = '/testApi'
+  } else if (env === 'production') {
+    proxyPath = '/prodApi'
+  }
+  return proxyPath
+}

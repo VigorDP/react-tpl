4c6fc4e28938aeab983f2860f4a58309a8e807e5
fengyinchao <fengyinchao@shimo.im>
2018-11-24 11:26:27 +0800

    chores: 整合 eslint 和 prettier，实现保存自动格式化

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
-import { syncHistoryWithStore } from 'react-router-redux'
-import { Provider } from 'react-redux'
-import App from 'containers/Main/App'
-import Text from 'containers/Text'
-import store from 'store'
-import Immutable from 'immutable'
-import createHashHistory from 'history/createHashHistory'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import App from 'containers/Main/App';
+import Text from 'containers/Text';
+import store from 'store';
+import Immutable from 'immutable';
+import createHashHistory from 'history/createHashHistory';
 
-let history = createHashHistory()
+let history = createHashHistory();
 syncHistoryWithStore(history, store, {
   selectLocationState(state) {
-    return state.get('routing')
+    return state.get('routing');
   }
-})
+});
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <Switch>
-        <Route path="/login" component={Text} />
-        <Route path="/" component={App} />
-      </Switch>
-    </Router>
+        <Route path="/login" component={Text} />{' '}
+        <Route path="/" component={App} />{' '}
+      </Switch>{' '}
+    </Router>{' '}
   </Provider>,
   document.getElementById('root')
-)
+);
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
   // module.hot.accept('./store/reducers', () => {
   //     const nextRootReducer = require('./store/reducers/index');
   //     store.replaceReducer(nextRootReducer);
